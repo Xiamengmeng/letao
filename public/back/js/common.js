0e7f986b3439e35a56aea9cc828e8699ed9b6f83
Xiamengmeng <940503239@qq.com>
2018-04-07 09:17:47 +0800

    主页面提交

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,40 @@
 /**
  * Created by mymy on 2018/4/6.
  */
+
+$(function(){
+// 禁用小环环
+  NProgress.configure({ showSpinner: false });
+
+// ajax 开始
+  $( document ).ajaxStart(function() {
+    // 进度条加载效果
+    NProgress.start();
+  });
+
+// ajax 结束
+  $( document ).ajaxStop( function() {
+    // 模拟网络延迟
+    setTimeout(function() {
+      // 进度条关闭
+      NProgress.done();
+    }, 500);
+  });
+
+
+$(".category").click(function(){
+  $(this).next().stop().slideToggle();
+  //console.log('jj');
+});
+
+
+//顶部菜单栏
+
+$(".icon_menu").click(function(){
+  //console.log("jj");
+  $('.side').toggleClass("hidemenu");
+  $('.main_top').toggleClass("hidemenu");
+  $('.main_content').toggleClass("hidemenu");
+});
+
+})
