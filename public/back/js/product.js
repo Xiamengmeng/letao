111b12f5a9193265d02a8d89fbb6e3e4f723b59b
Xiamengmeng <940503239@qq.com>
2018-04-08 22:02:54 +0800

    产品基本布局+模态框渲染

--- public/back/js/product.js
+++ public/back/js/product.js
@@ -1,3 +1,24 @@
 /**
  * Created by mymy on 2018/4/8.
  */
+$(function(){
+  var currentPage=1;
+  var pageSize=5;
+  $.ajax({
+    type:'get',
+    url:'/product/queryProductDetailList',
+    dataType:'json',
+    data:{
+      page:currentPage,
+      pageSize:pageSize
+    },
+    success:function(info){
+      console.log(info);
+      $(".lt_content tbody").html(template("productTpl",info));
+    }
+
+  });
+  $("#addBtn").click(function(){
+    $("#productModal").modal("show");
+  })
+})
